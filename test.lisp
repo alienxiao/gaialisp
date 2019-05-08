b04bd4c83c9dd48b54a552b4c740313bfbb7a37f
nicolassiu <nicolassiu2017@gmail.com>
2019-05-08 13:12:00 +0800

    update test.lisp

--- test.lisp
+++ test.lisp
@@ -1,9 +1,13 @@
 (progn
+	; print some string
   (print "hello")
   (print "world")
+	; test plus
   (print (+ 10 20))
-	(print "3^3 + 4^4 = ")
+	(print "3*3 + 4*4 = ")
+	; test multiply and plus
 	(print (+ (* 3 3) (* 4 4)))
+	; test minus and division
 	(print (- 10 6))
 	(print (/ 1 2)))
 ; this is a comment
