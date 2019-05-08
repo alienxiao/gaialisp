fa5b4b81809bd3c44317b424d3569d633ff486de
nicolassiu <nicolassiu2017@gmail.com>
2019-05-08 20:01:35 +0800

    add args supports

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	gaialisp "nicolas/gaialisp/lib"
+	"os"
 )
 
 func main() {
 	fmt.Println("gaialisp v0.0.1")
 	fmt.Println("developed by Nicolas Siu")
-	code := gaialisp.ReadFile("./test.lisp")
-	parser := gaialisp.NewParser(code)
-	//parser.Test()
-	root := parser.Parse()
+	if len(os.Args) > 1 {
+		fileName := os.Args[1]
+		code := gaialisp.ReadFile(fileName)
+		parser := gaialisp.NewParser(code)
+		//parser.Test()
+		root := parser.Parse()
 
-	vm := gaialisp.NewVM(root)
+		vm := gaialisp.NewVM(root)
 
-	vm.Eval()
+		vm.Eval()
+	}
 }
